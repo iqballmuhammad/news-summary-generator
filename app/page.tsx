9b8592709cb544788f1192fbe9eefece698dc53b
Muhammad Iqbal <muhammad.iqbal@shopee.com>
2023-10-11 00:06:28 +0700

    fix: loading styling

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ import { useState } from 'react';
 
 function SkeletonDemo() {
   return (
-    <div className='flex flex-col items-center space-x-4'>
+    <div className='flex flex-col items-center space-y-2'>
       <Skeleton className='h-4 w-[250px]' />
-      <Skeleton className='h-4 w-[250px]' />
-      <Skeleton className='h-4 w-[250px]' />
+      <Skeleton className='h-4 w-[225px]' />
+      <Skeleton className='h-4 w-[200px]' />
     </div>
   );
 }
