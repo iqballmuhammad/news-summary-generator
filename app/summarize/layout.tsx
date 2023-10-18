1c6737f7e9e30d6c3420e6ea75b3ed9b8d1a64cd
Muhammad Iqbal <iqbalmuhammad.18@gmail.com>
2023-10-18 18:40:17 +0700

    feat: add jsonld

--- app/summarize/layout.tsx
+++ app/summarize/layout.tsx
@@ -10,6 +10,13 @@ export const metadata = {
   description: 'Tools to generate summary from given article(s)'
 };
 
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'Organization',
+  url: 'https://dev.iqbl.me/summarize',
+  logo: 'https://dev.iqbl.me/favicon.ico'
+};
+
 export default function RootLayout({
   children
 }: {
@@ -17,6 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <body className={inter.className}>
         <div className='flex justify-center flex-col w-full h-screen max-w-md py-24 mx-auto stretch p-10'>
           {children}
