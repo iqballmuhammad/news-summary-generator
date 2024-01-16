eb5e2d48c3d0ba76b556cc861e6c02f3f4548088
Muhammad Iqbal <iqbalmuhammad.18@gmail.com>
2024-01-16 17:47:43 +0700

    fix: bot api response

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -1,10 +1,15 @@
 import { ISeatalkRequestBody } from '@/lib/types';
+import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
   const res = await req.json();
   console.log(res);
-  const {
-    event: { seatalk_challenge }
-  } = res as ISeatalkRequestBody;
-  return Response.json({ seatalk_challenge });
+  if (res.event) {
+    console.log(res);
+    const {
+      event: { seatalk_challenge }
+    } = res as ISeatalkRequestBody;
+    return NextResponse.json({ seatalk_challenge });
+  }
+  return NextResponse.json({'error': 'event element does not exist'})
 }
